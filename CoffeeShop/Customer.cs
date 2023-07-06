b9ad1bafadd774d7d66068fa2dd5ad59bbe3a9a8
skylarbsandler <skylarbsandler@gmail.com>
2023-07-06 15:32:02 -0600

    Added Customer Class and Tests

---
+++ CoffeeShop/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop
+{
+    public class Customer
+    {
+        public string Name;
+        public List<Order> Orders;
+
+        public  Customer(string name)
+        {
+            Name = name;
+            Orders = new List<Order>();
+        }
+
+        public void AddOrder(Order order)
+        {
+            Orders.Add(order);
+        }
+
+        public double TotalSpenct()
+        {
+            List<double> priceInCentsList = new List<double>();
+            foreach (Order order in Orders)
+            {
+                foreach (Item item in order.Items)
+                {
+                    priceInCentsList.Add(item.PriceInCents);
+                }
+            }
+            var sumDollars = priceInCentsList.Sum();
+            return sumDollars / 100;
+        }
+
+        public void ItemsOrdered()
+        {
+            foreach (Order order in Orders)
+            {
+                foreach (Item item in order.Items)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+    }
+}
---
+++ TestProject1/CustomerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoffeeShop;
+
+namespace TestProject1
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void Customer_Constructor_AssignsCorrectValues()
+        {
+            Customer customer1 = new Customer("Skylar");
+
+            Assert.Equal("Skylar", customer1.Name);
+            Assert.Empty(customer1.Orders);
+        }
+
+        [Fact]
+        public void Customer_AddOrder_AddsOrdertoCustomerOrderList()
+        {
+            Customer customer1 = new Customer("Skylar");
+
+            Item item1 = new Item("Latte", 375);
+            Item item2 = new Item("Cappuccino", 400);
+            Item item3 = new Item("Mocha", 450);
+            Item item4 = new Item("Caramel Macchiato", 500);
+
+            Order order1 = new Order();
+            Order order2 = new Order();
+
+            order1.AddItem(item1);
+            order1.AddItem(item2);
+            order2.AddItem(item3);
+            order2.AddItem(item4);
+
+            customer1.AddOrder(order1);
+            customer1.AddOrder(order2);
+
+            Assert.Equal(order1, customer1.Orders.First());
+            Assert.Equal(order2, customer1.Orders.Last());
+
+        }
+    }
+}
